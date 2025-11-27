01e4077a87093c79e89476090ec165e43b274cbb
Joe <heckj@hawaii.edu>
2025-11-26 23:35:54 -1000

    Create __init__.py

---
+++ cogs/__init__.py
@@ -0,0 +1,13 @@
+"""
+Cogs for Chub Status Bot Lite.
+"""
+
+from .status import StatusCog
+from .stats import StatsCog
+from .setup import SetupCog
+
+__all__ = [
+    'StatusCog',
+    'StatsCog',
+    'SetupCog',
+]
